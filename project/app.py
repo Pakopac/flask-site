fe07d94ccbbdec3c750ece92faec2d1be647614b
Lilian.Pacaud <Lilian.Pacaud@sky-real.com>
2020-12-02 11:22:45 +0100

    Set up for ghpage deployement

--- project/app.py
+++ project/app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from flask_flatpages import FlatPages
+from flask_frozen import Freezer
 from jinja2 import Template 
 import json
+
 app = Flask(__name__)
+app.config.from_pyfile('settings.py')
+pages = FlatPages(app)
+freezer = Freezer(app)
 
 # json importation
-books_file = "static/assets/books.json"
+books_file = "./project/static/assets/books.json"
 with open(books_file) as json_file:
     books_load = json.load(json_file)
 books_dump = json.dumps(books_load)
@@ -25,6 +31,6 @@ def book_by_id(id):
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-
+    
 if __name__ == '__main__':
     app.run(debug=True)
