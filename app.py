32c31a86e0e8563a1c0e5b778d7f81ce316a9191
Lilian.Pacaud <Lilian.Pacaud@sky-real.com>
2020-12-02 10:28:53 +0100

    Add project page

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
 from jinja2 import Template 
+import json
 app = Flask(__name__)
+
+# json importation
+books_file = "static/assets/books.json"
+with open(books_file) as json_file:
+    books_load = json.load(json_file)
+books_dump = json.dumps(books_load)
+books_json = json.loads(books_dump)
 
 @app.route('/')
 def home():
@@ -8,7 +16,11 @@ def home():
 
 @app.route('/project')
 def project():
-    return render_template('project.html')
+    return render_template('project.html', books=books_json)
+
+@app.route('/book/<id>')
+def book_by_id(id):
+    return render_template('book_id.html', books=books_json, id=id)
 
 @app.route('/contact')
 def contact():
